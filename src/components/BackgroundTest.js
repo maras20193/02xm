af6b512def44de9a75316a33ba76916b33353881
Łukasz Markowski <lucas20193@gmail.com>
2024-09-14 09:17:09 +0200

    [BASIC] add theme

---
+++ src/components/BackgroundTest.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const BackgroundTest = () => (
+  <div className="bg-gradient flex h-screen w-screen flex-col items-center justify-center">
+    <div className="h-full w-full p-14">
+      <div className="h-full w-full bg-[linear-gradient(90deg,_#1D1E2033_5%,_transparent_5%),linear-gradient(0deg,_#1D1E2033_5%,_transparent_5%)] bg-[length:28px_28px]"></div>
+    </div>
+  </div>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // Background colors
+        layout: {
+          primary: 'rgba(var(--color-layout-primary), <alpha-value>)',
+          secondary: 'rgba(var(--color-layout-secondary), <alpha-value>)',
+        },
+        // Text colors
+        text: {
+          primary: 'rgba(var(--color-text-primary), <alpha-value>)',
+          secondary: 'rgba(var(--color-text-secondary), <alpha-value>)',
+        },
+        // Accent colors (e.g., buttons, icons)
+        accent: {
+          primary: 'rgba(var(--color-accent-primary), <alpha-value>)',
+          secondary: 'rgba(var(--color-accent-secondary), <alpha-value>)',
+        },
       },
     },
   },
