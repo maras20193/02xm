241770f7ddcc0424ca10ad9978e911293ed4315c
Łukasz Markowski <lucas20193@gmail.com>
2024-09-21 18:19:24 +0200

    [SECTION] finish wip section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import { arrow, logoBlack, logoTextWhite } from '@/assets';
 import Image from 'next/image';
 import { Chakra_Petch } from 'next/font/google';
+import { BackgroundTest } from '@/components/BackgroundTest';
 
 const chakraPetch = Chakra_Petch({
   subsets: ['latin'],
@@ -11,14 +12,17 @@ const chakraPetch = Chakra_Petch({
 
 export default function Home() {
   return (
-    <main className="flex h-svh w-full flex-col bg-layout-primary p-2">
-      <div className="bg-gradient flex h-[60%] w-full flex-col items-center justify-center gap-8 sm:h-2/3">
-        <Image src={logoBlack} alt="02XM Logo" className="size-[150px]" />
-        <h1 className="border-b-[1px] border-solid border-text-secondary px-5 pb-4 text-3xl font-semibold uppercase text-text-secondary">
+    <main className="flex h-svh w-full flex-col overflow-hidden bg-layout-primary p-2">
+      <div className="bg-gradient relative flex h-[60%] w-full flex-col items-center justify-center gap-8 md:h-2/3">
+        <Image src={logoBlack} alt="02XM Logo" className="z-10 size-[150px]" />
+        <h1 className="z-10 border-b-2 border-solid border-text-secondary px-5 pb-4 text-3xl font-semibold uppercase text-text-secondary">
           Coming soon
         </h1>
+        <div className="absolute z-0 h-full w-full px-2 py-4">
+          <BackgroundTest />
+        </div>
       </div>
-      <div className=".border-dotted-squares relative mt-4 flex h-[40%] w-full flex-col items-center justify-center gap-4 border-t-2 border-dashed border-text-primary px-5 text-center text-sm text-text-primary sm:h-1/3 md:gap-5 lg:text-lg">
+      <div className=".border-dotted-squares relative mt-4 flex h-[40%] w-full flex-col items-center justify-center gap-4 border-t-2 border-dashed border-text-primary px-5 text-center text-sm text-text-primary md:h-1/3 md:gap-5 lg:text-lg">
         <p>Witaj na naszej stronie!</p>
         <p className="max-w-[300px] sm:max-w-[600px] md:max-w-none">
           Aktualnie jest w budowie...{' '}
--- src/components/BackgroundTest.js
+++ src/components/BackgroundTest.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export const BackgroundTest = () => (
-  <div className="bg-gradient flex h-screen w-screen flex-col items-center justify-center">
-    <div className="h-full w-full p-14">
-      <div className="h-full w-full bg-[linear-gradient(90deg,_#1D1E2033_5%,_transparent_5%),linear-gradient(0deg,_#1D1E2033_5%,_transparent_5%)] bg-[length:28px_28px]"></div>
-    </div>
+  <div className="h-full w-full">
+    <div className="h-full w-full bg-[linear-gradient(90deg,_#1D1E2033_5%,_transparent_5%),linear-gradient(0deg,_#1D1E2033_5%,_transparent_5%)] bg-[length:28px_28px] bg-[position:14px_14px]"></div>
   </div>
 );
